195d641418f44c9940881dc498259274d36fb307
calielian <calielian@proton.me>
2025-11-12 21:08:19 -0300

    Finished insert implemetation

--- src/mod.py
+++ src/mod.py
@@ -18,8 +18,8 @@ class InserirDados:
         # FRAME E WINDOW PRINCIPAL
         self.window = tk.Toplevel(parent)
         self.window.title("Inserir uma nova publicação")
-        self.window.minsize(400, 300)
-        self.window.maxsize(400, 300)
+        self.window.minsize(400, 350)
+        self.window.maxsize(400, 350)
         self.window.resizable(False, False)
 
         main_frame: ttk.Frame = ttk.Frame(self.window, padding="10")
@@ -48,27 +48,41 @@ class InserirDados:
         self.titulo_input: tk.Text = tk.Text(conteudo_frame, height=1, width=20)
         self.titulo_input.grid(row=1, column=1, pady=10, sticky="w")
 
+        self.titulo_input.bind('<KeyPress>', self.verificar_caractere_titulo)
+        self.titulo_input.bind('<KeyRelease>', self.verificar_caractere_titulo)
+
+        # LABEL E INPUT DO TIPO DO LIVRO
+
+        tipo_label: ttk.Label = ttk.Label(conteudo_frame, text="Tipo de livro:", font=('Arial', 12))
+        tipo_label.grid(row=2, column=0, padx=(0, 10), pady=10, sticky="w")
+
+        self.tipo_input: tk.Text = tk.Text(conteudo_frame, height=1, width=20)
+        self.tipo_input.grid(row=2, column=1, pady=10, sticky="w")
+
+        self.tipo_input.bind('<KeyPress>', self.verificar_caractere_tipo)
+        self.tipo_input.bind('<KeyRelease>', self.verificar_caractere_tipo)
+
         # LABEL E INPUT DA DATA DO TÍTULO
 
         data_label: ttk.Label = ttk.Label(conteudo_frame, text="Data de pub. do livro:", font=('Arial', 12))
-        data_label.grid(row=2, column=0, padx=(0, 10), pady=10, sticky="w")
+        data_label.grid(row=3, column=0, padx=(0, 10), pady=10, sticky="w")
 
         self.data_input: tk.Text = tk.Text(conteudo_frame, height=1, width=20)
-        self.data_input.grid(row=2, column=1, pady=10, sticky="w")
+        self.data_input.grid(row=3, column=1, pady=10, sticky="w")
 
         self.data_input.bind('<KeyPress>', self.verificar_caractere_data)
         self.data_input.bind('<KeyRelease>', self.verificar_caractere_data)
 
         # LABEL DE ERRO
-        self.erro_label: ttk.Label = ttk.Label(conteudo_frame, text="Teste", font=('Arial', 12), foreground="red", wraplength=350, justify="left", anchor="w")
-        self.erro_label.grid(row=3, column=0, columnspan=2, pady=10, sticky="we")
+        self.erro_label: ttk.Label = ttk.Label(conteudo_frame, text="", font=('Arial', 12), foreground="red", wraplength=350, justify="left", anchor="w")
+        self.erro_label.grid(row=4, column=0, columnspan=2, pady=10, sticky="we")
 
         # BOTÕES
 
         botoes_frame: ttk.Frame = ttk.Frame(main_frame)
         botoes_frame.pack(fill=tk.X, pady=(20, 0))
 
-        ok_button = ttk.Button(botoes_frame, text="Ok", command=lambda: self.Input(self.id_input,self.titulo_input, self.data_input))
+        ok_button = ttk.Button(botoes_frame, text="Ok", command=lambda: self.Input(self.id_input,self.titulo_input, self.tipo_input, self.data_input))
         ok_button.pack(side=tk.RIGHT, padx=(5,0))
 
         cancelar_button = ttk.Button(botoes_frame, text="Cancelar", command=self.window.destroy)
@@ -97,9 +111,26 @@ class InserirDados:
         
         return "break"
 
+    def verificar_caractere_titulo(self, event) -> None:
+        tam: int = len(self.titulo_input.get("1.0", "end-1c"))
+        LIMITE_CHAR: int = 80
+
+        if tam >= LIMITE_CHAR and event.keysym not in {"BackSpace", "Delete"}:
+            return 'break'
+    
+    def verificar_caractere_tipo(self, event) -> None:
+        tam: int = len(self.tipo_input.get("1.0", "end-1c"))
+        LIMITE_CHAR: int = 12
+
+        if tam >= LIMITE_CHAR and event.keysym not in {"BackSpace", "Delete"}:
+            return 'break'
+
     def verificar_formatacao_data(self, data: str) -> bool:
         i: int = 1
         char_formatacao: list = ['-', ' ', '/']
+
+        if len(data) != 10:
+            return False
 
         # formato correto: yyyy-mm-dd, yyyy mm dd, yyyy/mm/dd
         for char in data:
@@ -118,15 +149,15 @@ class InserirDados:
     def erro(self, texto_erro: str):
         self.erro_label.config(text=texto_erro)
 
-    def Input(self, id_text: tk.Text,titulo: tk.Text, data: tk.Text):
+    def Input(self, id_text: tk.Text,titulo: tk.Text, tipo: tk.Text, data: tk.Text):
         ID: str = id_text.get("1.0", "end-1c")
         TITULO: str = titulo.get("1.0", "end-1c")
+        TIPO: str = tipo.get("1.0", "end-1c")
         DATA: str = data.get("1.0", "end-1c")
 
-        if len(ID) == 0 or len(TITULO) == 0 or len(DATA) == 0:
-            pass
-        elif len(DATA) < 10:
-            pass
+        if len(ID) == 0 or len(TITULO) == 0 or len(TIPO) == 0 or len(DATA) == 0:
+            self.erro("Dados inválidos: algum(ns) campo(s) está(ão) vazio(s)")
+            return
         elif not self.verificar_formatacao_data(DATA):
             self.erro("Verifique se a formatação da data está correta: yyyy-mm-dd, yyyy mm dd ou yyyy/mm/dd.\nOu então verifique se digitou ano, mês ou dia válido.")
             return
@@ -134,10 +165,11 @@ class InserirDados:
         db = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="root",
+            password="serra",
             database="publicacao"
         )
 
         cursor = db.cursor()
 
-        cursor.execute("INSERT INTO titulos (ID_TITULO, TITULO_LIVRO, DATA_PUBLICACAO) VALUES (%s, %s, %s)", (ID, TITULO, DATA))
+        cursor.execute("INSERT INTO titulos (ID_TITULO, TITULO_LIVRO, TIPO_LIVRO, DATA_PUBLICACAO) VALUES (%s, %s, %s, %s)", (ID, TITULO, TIPO, DATA))
+        db.commit()
